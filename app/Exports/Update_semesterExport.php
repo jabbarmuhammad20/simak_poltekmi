27c399cda9a0c3ba9836fc5c50eef5d72c0ddf69
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-04-23 09:57:29 +0700

    index + 1 for export update_semester

--- app/Exports/Update_semesterExport.php
+++ app/Exports/Update_semesterExport.php
@@ -3,12 +3,15 @@
 namespace App\Exports;
 
 use App\Models\Mahasiswa;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class Update_semesterExport implements FromCollection, WithMapping, WithHeadings
 {
+    private $index = 0; // Initialize row number
+
     /**
     * @return \Illuminate\Support\Collection
     */
@@ -17,31 +20,31 @@ class Update_semesterExport implements FromCollection, WithMapping, WithHeadings
         return Mahasiswa::all();
     }
 
-    public function map($mahasiswa):array
-        {
-            return [
-                //data yang dari kolom tabel database yang akan diambil
-                $mahasiswa->id,
-                $mahasiswa->tahun_masuk,
-                $mahasiswa->npm,
-                $mahasiswa->User->name,
-                $mahasiswa->programstudi->programstudi,
-                $mahasiswa->semester,
-                $mahasiswa->aktif,
-            ];
-        }
+    public function map($mahasiswa): array
+    {
+        $this->index++;
 
-        public function headings():array
-        {
-            return [
-                //pastikan urut dan jumlahnya sesuai dengan yang ada di mapping-data atau table di database
-                'No',
-                'Tahun Masuk',
-                'NPM',
-                'Nama',
-                'Program Studi',
-                'Semester',
-                'Aktif',
-            ];
-        }
+        return [
+            $this->index,
+            $mahasiswa->tahun_masuk,
+            $mahasiswa->npm,
+            $mahasiswa->User->name,
+            $mahasiswa->programstudi->programstudi,
+            $mahasiswa->semester,
+            $mahasiswa->aktif,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Tahun Masuk',
+            'NPM',
+            'Nama',
+            'Program Studi',
+            'Semester',
+            'Aktif',
+        ];
+    }
 }
